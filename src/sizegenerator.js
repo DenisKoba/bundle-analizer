7db596cfe4a496885fc6bdb51f0a731f72b5ce40
deniskoba <d.koba.nsc@gmail.com>
2020-04-05 21:17:11 +0300

    changes size generator

--- src/sizegenerator.js
+++ src/sizegenerator.js
@@ -3,49 +3,48 @@ const fs = require("fs")
 const chalk = require('chalk');
 
 const getBundleSize = async (path) => {
-  const sunc = glob.sync(`${path}/**/*.js`)
-    .map(filename => {
-      const stats = fs.statSync(filename)
-      return stats["size"]
-    })
-    .reduce((sum, current) => { return sum + current }, 0)
-  return Promise.resolve(sunc)
+    const sunc = glob.sync(`${path}/**/*.js`)
+        .map(filename => {
+            const stats = fs.statSync(filename)
+            return stats["size"]
+        })
+        .reduce((sum, current) => { return sum + current }, 0)
+
+    return Promise.resolve(sunc)
 }
 
 const fetch = require('node-fetch');
 
-getBundleSize('./lib').then(data => analizeBundleSize(data) )
+getBundleSize('./dist').then(data => analizeBundleSize(data) )
 
 const twirlTimer = () => {
-  const P = ["\\", "|", "/", "-"];
-  let x = 0;
-  return setInterval(function() {
-    process.stdout.write(chalk.blue("\r" + P[x++]));
-    x &= 3;
-  }, 250)
+    const P = ["\\", "|", "/", "-"];
+    let x = 0;
+    return setInterval(function() {
+        process.stdout.write(chalk.blue("\r" + P[x++]));
+        x &= 3;
+    }, 250)
+}
+
+const exit = (color, message, error = '') => {
+    process.exit(console.log(chalk[color](message, error)))
 }
 
 const analizeBundleSize = (nextSize) => {
-  twirlTimer()
-  fetch('http://localhost:8082/build/analize', {
-    method: 'post',
-    body: JSON.stringify({ nextSize, repo: 'any' }),
-    headers: { 'Content-Type': 'application/json' },
-  })
-    .then(res => res.json())
-    .then((json) => {
-      if (json.status) {
-        return process.exit(
-          console.log(
-            chalk.green('success')
-          )
-        )
-      }
-
-      process.exit(
-        console.log(
-          chalk.red('fail')
-        )
-      )
+    twirlTimer()
+    fetch('https://st-bundle-size.herokuapp.com/build/analyze', {
+        method: 'post',
+        body: JSON.stringify({ nextSize, repo: 'mobile' }),
+        headers: { 'Content-Type': 'application/json' },
     })
+        .then(res => res.json())
+        .then((json) => {
+            console.log(json)
+            if (json.status) {
+                return exit('green', 'success')
+            }
+
+            exit('red', 'fail')
+        })
+        .catch((error) => exit('red', 'fail', error))
 }
